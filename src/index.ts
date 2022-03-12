b0bcb15c431928997b24ae9aa638e19ce981970a
Ivan Proskuriakov <snowflax@ya.ru>
2022-03-12 17:47:13 +0300

    feat: Server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { Telegraf } from 'telegraf';
-import dotEnv from 'dotenv'; // .env file
-dotEnv.config();
+import dotenv from 'dotenv'; // .env file
+dotenv.config();
 
 // @ts-ignore
-const port = process.env.PORT;
-const apiKey = process.env.APIKEY;
-console.log(apiKey);
+const { PORT, APIKEY } = process.env;
+console.log(APIKEY);
 
-const bot = new Telegraf(apiKey as string);
+const bot = new Telegraf(APIKEY as string);
 
 bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
